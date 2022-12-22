7b30d87035ff7a08cc57f72617ccd002a016f5b1
Stephen Zhang <yicheng123456@outlook.com>
2022-12-23 01:54:49 +0800

    bugfix for keymaps with prefix Ctrl
        [+] replace <cr> to <CR>

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -6,18 +6,18 @@ local defs = {
         opts = {
             group = nil,
             pattern = '*',
-            callback = function ()
-                vim.highlight.on_yank({higroup = 'IncSearch', timeout = 300})
-            end
-        }
+            callback = function()
+                vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 300 })
+            end,
+        },
     },
 }
 
-event.setup = function ()
+event.setup = function()
     vim.api.nvim_create_augroup('UserDefined', { clear = true })
 
     for _, def in ipairs(defs) do
-        def.opts.group = "UserDefined"
+        def.opts.group = 'UserDefined'
         vim.api.nvim_create_autocmd(def.event, def.opts)
     end
 end
